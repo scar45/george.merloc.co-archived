4443e228cf5857421b5e348b030ec5ac7af6ff74
George Merlocco <george@merloc.co>
2015-11-09 20:29:55 -0500

    Completely refactored / optimized gulp setup

--- README.md
+++ README.md
@@ -4,29 +4,30 @@
 This is my personal portfolio website that I've chosen to open source so that you can see exactly how I work. This project uses the [gulp.js](https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md) build system and a host of dependencies to optimize all output. Some of the kits included are:
 
 - SASS w/Source Maps (node-sass + gulp-sass + gulp-sourcemaps)
-- jQuery (w/ gulp-jquery)
-- Image Minifier (gulp-imagemin)
-- CSS + JS File Includes (gulp-include)
+- jQuery (via bower)
+- HTML + CSS + JS + Image Minifier (gulp-minify-html + gulp-minify-css + gulp-uglify + gulp-imagemin)
+- CSS + JS File Includes (gulp-file-include)
 - [Browsersync](http://www.browsersync.io) for live coding (browser-sync)
+
+## Requirements
+
+- [Node.js](http://nodes.org)
+- [Gulp.js](http://gulpjs.com)
+- [Bower](http://bower.io)
 
 ### Getting it running
 --- 
 **Install dependencies:**
 ```
 npm install
+bower install jquery
 ```
-**Build first to ensure all's well:**
+**Start a first build, then spawn webserver for live coding (browser-sync):**
 ```
-gulp release
+gulp
 ```
-- This should dump files in a ./build directory, which you can serve up.
+- This will dump compiled/processed files in a ./build directory, which will then be served by browser-sync.
 
-**Setup a watch environment to build and refresh a live server:**
-```
-gulp liveCoding
-```
-- This will run a live server (Browsersync) on port 3000, with a Web UI on port 3001.
-    
 ---
 
 ...and that's about it! A live copy of this site should be found @ http://george.merloc.co
--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,40 @@
 var gulp = require('gulp'),
-    del = require('del'),
-    combiner = require('stream-combiner2'),
-    runSequence = require('run-sequence'),
-    includeSources = require("gulp-include-source"),
-    sourcemaps = require('gulp-sourcemaps'),
-    fileInclude = require("gulp-file-include"),
-    cssmin = require("gulp-cssmin"),
-    sass = require('gulp-sass'),
-    jquery = require('gulp-jquery'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
     browserSync = require('browser-sync'),
-    reload = browserSync.reload;
+    concat = require("gulp-concat"),
+    del = require('del'),
+    fileInclude = require("gulp-file-include"),
+    imagemin = require('gulp-imagemin'),
+    includeSources = require("gulp-include-source"),
+    minifyHTML = require("gulp-minify-html"),
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify');
 
 var config = {
     paths: {
         html: {
-            src:  "src/**/*.html",
-            dest: "build"
+            src:  "sauce/template/**/*.html",
+            partialsrc:  "sauce/template/partials/**/*.phtml",
+            dest: "build",
         },
         javascript: {
-            src:  ["src/js/**/*.js"],
-            dest: "build/js"
-        },
-        css: {
-            src: ["src/css/**/*.css"],
-            dest: "build/css"
+            src:  ["sauce/js/**/*.js"],
+            dest: "build/js",
+            vendorSrc: "bower_components/jquery/dist/jquery.min.js",
+            vendorDest: "build/js/vendor/"
         },
         images: {
-            src: ["src/images/**/*.jpg", "src/images/**/*.jpeg", "src/images/**/*.png"],
+            src: ["sauce/images/**/*.jpg", "sauce/images/**/*.jpeg", "sauce/images/**/*.png"],
             dest: "build/images"
         },
-        less: {
-            src: ["src/less/**/*.less", "!src/less/includes/**"],
+        sass: {
+            src: ["sauce/sass/**/*.scss", "!sauce/sass/includes/**"],
             dest: "build/css"
         },
-        bower: {
-            src: "bower_components",
-            dest: "build/lib"
-        },
-        verbatim: {
-            src: ["src/manifest.json", "src/favicon.png"],
+        untouched: {
+            src: ["sauce/favicon.png"],
             dest: "build"
         }
     }
@@ -51,106 +44,79 @@ gulp.task('clean', function() {
     return del('./build');
 });
 
-
-gulp.task('html', function() {
-    runSequence('page-home','page-portfolio');
+gulp.task("untouched", function(){
+    gulp.src(config.paths.untouched.src)
+        .pipe(gulp.dest(config.paths.untouched.dest));
 });
 
-gulp.task('page-home', function() {
-    return gulp.src( './sauce/template/index.html' )
-    .pipe( fileInclude({
-        prefix: '@@',
-        basepath: '@file',
-        context: {
-            name: 'activeNav'
-        }
-    }))
-    .pipe( includeSources({cwd: 'build/'}) )
-    .pipe(gulp.dest('./build/'));
+gulp.task('jquery', function() {
+    return gulp.src(config.paths.javascript.vendorSrc)
+        .pipe(gulp.dest(config.paths.javascript.vendorDest))
 });
 
-gulp.task('page-portfolio', function() {
-    return gulp.src( './sauce/template/portfolio.html' )
-    .pipe( fileInclude({
-        prefix: '@@',
-        basepath: '@file',
-        context: {
-            name: 'activeNav'
-        }
-    }))
-    .pipe( includeSources({cwd: 'build/'}) )
-    .pipe(gulp.dest('./build/'));
+gulp.task('html', ['sass', 'scripts', 'images'], function() {
+    return gulp.src(config.paths.html.src)
+        .pipe( fileInclude({
+            prefix: '@@',
+            basepath: '@file',
+            context: {
+                name: 'activeNav'
+            }
+        }))
+        .pipe(includeSources({cwd: 'build/'}))
+        .pipe(minifyHTML())
+        .pipe(gulp.dest(config.paths.html.dest));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(config.paths.sass.src)
+        .pipe(sourcemaps.init())
+            .pipe(sass().on('error', sass.logError))
+            .pipe(minifyCss())
+            .pipe(rename({ extname: '.min.css' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.paths.sass.dest))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(config.paths.javascript.src)
+        .pipe(sourcemaps.init())
+            .pipe(concat("gm.min.js"))
+            .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.paths.javascript.dest));
 });
 
 gulp.task('images', function() {
     // Quick 5s delay to let Gimp/editor finish writing the image
+    //setTimeout(function() {
+        return gulp.src(config.paths.images.src)
+            .pipe(imagemin({
+                progressive: true
+            }))
+            .pipe(gulp.dest(config.paths.images.dest));
+    //}, 5000);
+});
+
+gulp.task('liveCoding', function() {
+    // Quick 3s delay to allow all tasks to complete
     setTimeout(function() {
-        gulp.src(['sauce/images/*.jpg', './sauce/images/*.png'])
-            .pipe(imagemin({ progressive: true }))
-            .pipe(gulp.dest('./build/images'));
-    }, 5000);
+        browserSync({
+           server: {
+               baseDir: './build'
+           }
+        });
+    }, 3000);
+
 });
 
-gulp.task('sass', function () {
-    gulp.src('./sauce/css/**/*.scss')
-    .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('.', {includeContent: false, debug: true}))
-    .pipe(gulp.dest('./build/css/'));
-});
+gulp.task('build', ['untouched', 'jquery', 'html']);
 
-gulp.task('sassNoMap', function () {
-    gulp.src('./sauce/css/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css/'));
-});
-
-gulp.task('scripts', ['jquery'], function() {
-    var combined = combiner.obj([
-        gulp.src("./sauce/js/gm.js"),
-        //uglify(),
-        //rename({ extname: '.min.js' }),
-        gulp.dest("./build/js"),
-
-        // TODO: Look into why sourcemap is not generated
-        sourcemaps.init( {loadmaps: false} ),
-            gulp.dest("./build/js"),
-        sourcemaps.write('.', {includeContent: false, debug: true})
-    ]);
-    combined.on('error', console.error.bind(console));
-
-    return combined;
-});
-
-gulp.task('jquery', function () {
-    return jquery.src({
-        release: 2, //jQuery v1 or v2
-        flags: ['-deprecated', '-event/alias', '-ajax/script', '-ajax/jsonp', '-exports/global']
-    })
-    // creates ./build/js/vendor/jquery.custom.js
-    .pipe(gulp.dest('./build/js/vendor/'));
-});
-
-gulp.task('watch', function () {
-    gulp.watch(['sauce/images/**/*.jpg', 'sauce/images/**/*.png'], ['images']);
-    gulp.watch(['sauce/css/**/*.scss'], ['sass']);
-    gulp.watch(['sauce/template/*.html', 'sauce/template/partials/*.phtml'], ['html']);
-});
-
-// Main task to run during development -- starts a server and watches for changes
-gulp.task('liveCoding', ['sass', 'images', 'scripts', 'html', 'watch'], function() {
-    browserSync({
-        server: {
-            baseDir: './build'
-        }
-    });
-    // Refreshes the dev URL
-    gulp.watch(['build/css/**/*.css', 'build/js/**/*.js', 'build/images/**/*.jpg', 'build/images/**/*.png', 'build/**/*.html'], reload);
-});
-
-gulp.task('release', ['clean'], function() {
-
-    // Note that run-sequence may be deprecated once gulp 4.0 is released with magic
-    //      support for defining task dependencies in series or in parallel.
-    runSequence('sassNoMap', 'images', 'scripts', 'html');
+gulp.task('default', ['build', 'liveCoding'], function(){
+    gulp.watch([config.paths.html.src, config.paths.html.partialsrc], ['html', browserSync.reload]);
+    gulp.watch(config.paths.sass.src, ['sass', browserSync.reload]);
+    gulp.watch(config.paths.javascript.src, ['scripts', browserSync.reload]);
+    gulp.watch(config.paths.images.src, ['images', browserSync.reload]);
+    gulp.watch(config.paths.untouched.src, ['untouched', browserSync.reload]);
 });
